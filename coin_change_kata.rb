5cbcb8c0ced663c7200590bab5146d02104ed913
John Erickson <ericksonjohn08@hotmail.com>
2016-08-15 16:39:24 -0400

    added coin_change_kata and test_coin_change

---
+++ coin_change_kata.rb
@@ -0,0 +1,12 @@
+Coin changer kata
+
+Write a function that takes a number of cents, and that returns a hash containing the least number of coins
+needed to generate the value of cents.
+# Write a function which has a has including all keys/values
+
+eg. If you have 11 cents you should return {:dime => 1, :penny => 1};
+eg. If you have 31 cents you should return {:quarter => 1, :nickel => 1, :penny => 1};
+eg. If you have 93 cents {:quarter => 3, :dime => 1, nickel => 1, :penny => 3}
+
+Start a new repo
+Build up your coin_changer using tests. Commit to git after every test.

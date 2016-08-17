a7fb2c83137bcfaf3c421f79917bc6ab30127274
John Erickson <ericksonjohn08@hotmail.com>
2016-08-17 12:52:42 -0400

    changed test from hash to array. Both tests pass, one passes true and the other passes false

--- coin_change_kata.rb
+++ coin_change_kata.rb
@@ -1,8 +1,6 @@
 # Coin changer kata
 
-# Write a function that takes a number of cents, and that returns a hash containing the least number of coins
-# needed to generate the value of cents.
-# # Write a function which has a has including all keys/values
+# Write a function that takes a number of cents, and that returns a hash containing the least number of coins needed to generate the value of cents.
 
 # eg. If you have 11 cents you should return {:dime => 1, :penny => 1};
 # eg. If you have 31 cents you should return {:quarter => 1, :nickel => 1, :penny => 1};
@@ -11,12 +9,25 @@
 # Start a new repo
 # Build up your coin_changer using tests. Commit to git after every test.
 
-def test_hash_values?(amount, currency)
-	# amount = 1 or "one"
-	# currency = { :penny => 1, :nickel => 5, :dime => 10, :quarter => 25 }
-	if currency[:penny] == amount
-		true
-	else
-		false
-	end
+def amount_in_array?(test_amount, amounts)
+	# amounts = [9.50, 0.96, 0.75, 3.06, 5.25]
+	# test_amount = 9.50
+	amounts.include?(test_amount)
 end
+
+
+
+
+
+
+
+# def test_hash_values?(amount, currency)
+# 	# amount = 1 or "one"
+# 	# currency = { :penny => 1, :nickel => 5, :dime => 10, :quarter => 25 }
+
+# 		if currency[:penny] == amount
+# 			true
+# 		else
+# 			false
+# 		end
+# end
--- test_coin_change.rb
+++ test_coin_change.rb
@@ -1,13 +1,10 @@
-# Assignment: Coin Changer:
+# Coin changer kata
 
-# Write a function that takes a number of cents, and that returns a hash containing the least 
-# number of coins needed to generate the value of cents.
+# Write a function that takes a number of cents, and that returns a hash containing the least number of coins needed to generate the value of cents.
 
-# eg. If you have 11 cents you should return {:dime => 1, :penny =>1}
-# eg. If you have 31 cents you should return {:quarter => 1, :nickel => 1, :penny => 1}
-# eg. If 93 cents {:quarter => 3, :dime => 1, :nickel => 1, :penny => 3}
-
-# Build up your coin_changer using tests. Commit to git after every test.
+# eg. If you have 11 cents you should return {:dime => 1, :penny => 1};
+# eg. If you have 31 cents you should return {:quarter => 1, :nickel => 1, :penny => 1};
+# eg. If you have 93 cents {:quarter => 3, :dime => 1, nickel => 1, :penny => 3}
 
 require "minitest/autorun"
 require_relative "coin_change_kata.rb" 
@@ -15,16 +12,30 @@ require_relative "coin_change_kata.rb"
 
 class TestTicket < Minitest::Test 
 
-	def test_if_hash_includes_amount_return_true
-		currency = { :penny => 1, :nickel => 5, :dime => 10, :quarter => 25 }
-		amount = 1
-		assert_equal(true, test_hash_values?(amount, currency))
+	def test_if_function_returns_true
+		# currency = [["Quarters", 0.25], ["Dimes", 0.10], ["Nickels", 0.05], ["Pennies", 0.01]]
+		amounts = [9.50, 0.96, 0.75, 3.06, 5.25]
+		test_amount = 9.50
+		assert_equal(true, amount_in_array?(test_amount, amounts))
 	end
 
-	def test_if_hash_does_not_include_amount_return_false
-		currency = { :penny => 1, :nickel => 5, :dime => 10, :quarter => 25 }
-		amount = "one"
-		assert_equal(false, test_hash_values?(amount, currency))
+	def test_if_function_returns_false
+		# currency = [["Quarters", 0.25], ["Dimes", 0.10], ["Nickels", 0.05], ["Pennies", 0.01]]
+		amounts = [9.50, 0.96, 0.75, 3.06, 5.25]
+		test_amount = 9.51
+		assert_equal(false, amount_in_array?(test_amount, amounts))
 	end
 
 end
+
+	# def test_if_hash_includes_amount_return_true
+	# 	currency = { :penny => 1, :nickel => 5, :dime => 10, :quarter => 25 }
+	# 	amount = 1
+	# 	assert_equal(true, test_hash_values?(amount, currency))
+	# end
+
+	# def test_if_hash_does_not_include_amount_return_false
+	# 	currency = { :penny => 1, :nickel => 5, :dime => 10, :quarter => 25 }
+	# 	amount = "one"
+	# 	assert_equal(false, test_hash_values?(amount, currency))
+	# end

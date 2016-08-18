4de5ec041a3fb55aacd901fb4bb928ea3f26b429
John Erickson <ericksonjohn08@hotmail.com>
2016-08-18 11:05:12 -0400

    refactored code

--- coin_class_kata.rb
+++ coin_class_kata.rb
@@ -1,6 +1,7 @@
 # Coin changer kata
 
-# Write a function that takes a number of cents, and that returns a hash containing the least number of coins needed to generate the value of cents.
+# Write a function that takes a number of cents, and that returns a hash containing the least 
+# number of coins needed to generate the value of cents.
 
 # eg. If you have 11 cents you should return {:dime => 1, :penny => 1};
 # eg. If you have 31 cents you should return {:quarter => 1, :nickel => 1, :penny => 1};
@@ -9,23 +10,40 @@
 def generate_change(cents)
 	change = {}
 
-	if cents > 15
+	if cents >= 10
 		change[:dime] = 1
+		cents = cents - 10
+	end
+
+	if cents >= 5
 		change[:nickel] = 1
-		change[:penny] = cents - 15
-	elsif cents > 10
-		change[:dime] = 1
-		change[:penny] = cents - 10
-	elsif cents == 10
-		change[:dime] = 1
-	elsif cents > 5
-		change[:nickel] = 1
-		change[:penny] = cents - 5
-	elsif cents == 5
-		change[:nickel] = 1
-	elsif cents > 0
+		cents = cents - 5
+	end
+
+	if cents >= 1
 		change[:penny] = cents
 	end
+
+
+
+
+	# if cents > 15
+	# 	change[:dime] = 1
+	# 	change[:nickel] = 1
+	# 	change[:penny] = cents - 15
+	# elsif cents > 10
+	# 	change[:dime] = 1
+	# 	change[:penny] = cents - 10
+	# elsif cents == 10
+	# 	change[:dime] = 1
+	# elsif cents > 5
+	# 	change[:nickel] = 1
+	# 	change[:penny] = cents - 5
+	# elsif cents == 5
+	# 	change[:nickel] = 1
+	# elsif cents > 0
+	# 	change[:penny] = cents
+	# end
 	change
 
 end

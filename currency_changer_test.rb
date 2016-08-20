120f6928edca3c2ed93ceab74fde5ad0a081a6e1
John Erickson <ericksonjohn08@hotmail.com>
2016-08-20 14:51:45 -0400

    Added 3 additional tests; :Dime, :Nickel, and :Penny. I received 3 failures; actual and expected did not match

--- currency_changer_test.rb
+++ currency_changer_test.rb
@@ -6,12 +6,29 @@ class TestCurrencyChanger < Minitest::Test
 # Class name MUST start w/ a capital letter and uses camelCase naming conventions
 # < Minitest::Test is boilerplate and is case sensative
 
-	def test_returns_empty_hash # function MUST start with 'test' and is case sensative
-		assert_equal({}, generate_currency(0))
-	end
+	# def test_returns_hash_with_key_quarter_value_two_when_passed_argument_fifty
+	# 	# function MUST start with 'test' and is case sensative
+	# 	assert_equal({:Quarter => 2}, generate_currency(50))
+	# end
 
 	def test_returns_hash_with_key_quarter_when_passed_argument_twenty_five
 		assert_equal({:Quarter => 1}, generate_currency(25))
+	end
+
+	def test_returns_hash_with_key_dime_when_passed_argument_ten
+		assert_equal({:Dime => 1}, generate_currency(10))
+	end
+
+	def test_returns_hash_with_key_nickel_when_passed_argument_five
+		assert_equal({:Nickel => 1}, generate_currency(5))
+	end
+
+	def test_returns_hash_with_key_penny_when_passed_argument_one
+		assert_equal({:Penny => 1}, generate_currency(1))
+	end
+
+	def test_returns_empty_hash 
+		assert_equal({}, generate_currency(0))
 	end
 
 end
